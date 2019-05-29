7423f7d2cf75949d73213c99bd3fd481fd2de137
Daniela <daniela.gallardo.reyes@gmail.com>
2019-05-29 18:38:52 +0200

    Update parte_3 (posiblesMEJORAS).sql

--- parte_3 (posiblesMEJORAS).sql
+++ parte_3 (posiblesMEJORAS).sql
@@ -1,6 +1,6 @@
 /*
 * TRABAJO FINAL M2 
-* PARTE 3 - SCRIPT parte_3(posiblesMEJORAS).sqñ
+* PARTE 3 - SCRIPT parte_3(posiblesMEJORAS).sql
 * procedimientos, funciones y triggers que se pueden/necesitan mejorar
 * autores: Daniela Gallardo, Carlos Masana, Artur Marin 
 */
@@ -36,4 +36,47 @@
 	select * from client;
 
 	--OBSERVACIÓN: no borra a los clientes que tienen comandas registradas
+
+	/* TRIGGER: para cada empleado por cada 10 productos 
+	que venda (sin importar el tipo de producto ni la comanda): 
+		* Se registra en la tabla ptos_por_ventas el id del empleado, 
+		la cantidad total de productos vendidos y el puntaje acumulado a la fecha.
+		* cada 10 productos vendidos se obtienen 5 puntos
+	*/
+
+-- creamos la tabla ptos_por_ventas
+CREATE TABLE ptos_por_venta(
+emp_no NUMBER(4) not null,
+fecha date not null,
+quantitat_prod Number(9) not null,
+puntaje NUMBER(9) null,
+    CONSTRAINT ptos_por_venta_pk
+        PRIMARY KEY (emp_no,client_cod),
+    CONSTRAINT ptos_por_venta_fk_emp
+		FOREIGN KEY (emp_no)
+		REFERENCES EMP (EMP_NO)
+);
+
+--hacemos el select que me mostraría campos que necesito guardarme en la nueva tabla
+SELECT client.REPR_COD as emp_no, sum(DETALL.QUANTITAT) 
+FROM (((comanda
+    INNER JOIN detall
+    ON COMANDA.COM_NUM= DETALL.COM_NUM)
+    INNER JOIN client
+    ON comanda.client_cod= client.client_cod)
+    INNER JOIN EMP
+    ON CLIENT.REPR_COD= EMP.EMP_NO)
+    group by client.REPR_COD;
+
+-- compruebo si el select anterior me está sumando correctamente 
+	--(sumando los resultados por tipo de producto)
+SELECT client.REPR_COD as emp_no, detall.prod_num, DETALL.QUANTITAT
+FROM (((comanda
+    INNER JOIN detall
+    ON COMANDA.COM_NUM= DETALL.COM_NUM)
+    INNER JOIN client
+    ON comanda.client_cod= client.client_cod)
+    INNER JOIN EMP
+    ON CLIENT.REPR_COD= EMP.EMP_NO)
+    order by client.REPR_COD;
 
